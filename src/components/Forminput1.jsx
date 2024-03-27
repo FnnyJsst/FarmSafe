49e0cb21788509a32af5850a1485fc706074226d
FnnyJsst <fanny.jousset@gmail.com>
2024-03-27 13:36:52 +0100

    started form on login page

---
+++ src/components/Forminput1.jsx
@@ -0,0 +1,12 @@
+import './Forminput1.css';
+
+function Forminput1 () {
+    return (
+        <div>
+            <input type='text'className='form-input-1'/>
+        </div>    
+    )
+
+}
+
+export default Forminput1;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,17 @@
 import './Login.css';
+import Forminput1 from '../components/Forminput1';
 
 function Login () {
  return (
     <div>
         <section className='gray-section'>
-            <h1>Get started</h1>
+            <div className='login-form'>
+                <h1>Get started</h1>
+                <form>
+                    <Forminput1 />
+                    <Forminput1 />
+                </form>
+            </div>
         </section>
         <img src='/src/assets/corn-field.jpg'/>
     </div>
